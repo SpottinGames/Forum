484f73d7bf376704ba143c89df1bddae9a3937d6
SpottinGames <ryan.spottingames@gmail.com>
2018-05-05 12:59:23 +0200

    V01.03
    Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SpottinGames Forum
                 </div>
 
                 <div class="links">
